4fbeb1dac894589ec80b98448d8ecdd995de239d
Muskan Goyal <muskangoyal1711@gmail.com>
2025-07-01 17:35:30 +0530

    Add matrix testing on multiple Python versions

--- test_app.py
+++ test_app.py
@@ -1,13 +1,7 @@
 from app import add, subtract 
 
 def test_data():
-    assert add(2,3) == 5
-
+    assert add(2, 3) == 5
 
 def test_subtract():
-    assert subtract(5,2) == 3
-
-
-# test_app.py
-# def test_add():
-#     assert add(2, 3) == 999  # Wrong on purpose
+    assert subtract(5, 2) == 3
